f461edd920ec18d9f754cce7a0508c4a24b3d10e
minhnguyenwp <minhnguyenwp@gmail.com>
2016-01-25 12:08:22 +0700

    M

--- www/js/app.js
+++ www/js/app.js
@@ -2,11 +2,7 @@
 var config = {
   path : {
       'baseURL'               : 'https://api-01.sg-benefits.com',
-<<<<<<< HEAD
       //'baseURL'               : 'https://api.sg-benefits.com',
-=======
-      //'baseURL'             : 'https://api.sg-benefits.com',
->>>>>>> origin/app2
 
       'clients'               : '/organisations',
       'client'                : '/organisations/:org_id',
