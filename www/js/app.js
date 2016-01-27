8e6c2909e33aad4ceb9783e415acd6579fbe07cb
minhnguyenwp <minhnguyenwp@gmail.com>
2016-01-27 14:58:10 +0700

    M

--- www/js/app.js
+++ www/js/app.js
@@ -1,8 +1,8 @@
 // Ionic Starter App
 var config = {
   path : {
-      'baseURL'               : 'https://api-01.sg-benefits.com',
-      //'baseURL'               : 'https://api.sg-benefits.com',
+      //'baseURL'               : 'https://api-01.sg-benefits.com',
+      'baseURL'               : 'https://api.sg-benefits.com',
 
       'clients'               : '/organisations',
       'client'                : '/organisations/:org_id',
